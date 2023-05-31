cfa4414a4860e099966af509a82090f26692b2e8
Morjick <matvey200171@gmail.com>
2023-05-31 15:36:14 +0300

    add settings

--- src/settings/settings.controller.ts
+++ src/settings/settings.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Post,
+  Query,
+  Res,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { SettingsService } from './settings.service';
 
+@ApiTags('Настройки')
 @Controller('settings')
-export class SettingsController {}
+export class SettingsController {
+  constructor(private settingReposity: SettingsService) {}
+
+  @Post('change-password')
+  async changePassword(@Body() body, @Headers() Headers, @Res() res) {
+    return await this.settingReposity.changePassword(body, res);
+  }
+}
