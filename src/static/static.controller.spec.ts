5e3dc34492018d1aaa6f6423e507e6201121f090
Vafil42 <123875901+Vafil42@users.noreply.github.com>
2023-06-29 10:14:39 +0300

    fix static.controller.spec, fix static.controller

--- src/static/static.controller.spec.ts
+++ src/static/static.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StaticController } from './static.controller';
+import { StaticService } from './static.service';
 
 describe('StaticController', () => {
   let controller: StaticController;
@@ -7,6 +8,7 @@ describe('StaticController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StaticController],
+      providers: [StaticService],
     }).compile();
 
     controller = module.get<StaticController>(StaticController);
